ccfd2b29e8e4c4827251c81ce5d6d66d7bc98c9f
kalyaniasthana <kalyaniasthana@gmail.com>
2020-03-09 12:00:37 +0530

    plots with stats

--- martin_dca/calculate_dca_scores.m
+++ martin_dca/calculate_dca_scores.m
@@ -8,17 +8,26 @@ function [score_train, score_test] = calculate_dca_scores(fasta_train,fasta_test
 [eij,hi] = coupling_b( C, Pi_pcred, N, q );
 score_train = score_fct( fasta_train , eij, hi, N, q);
 score_test = score_fct( fasta_test , eij, hi, N, q);
+
 cf = strcat('/media/Data/consensus/all_consensus_sequences/', accession);
 consensus_file = strcat(cf, '_consensus.fasta');
 score_consensus = score_fct( consensus_file, eij, hi, N, q);
-fprintf('%d\n', score_consensus(1));
-fprintf('%d\n', score_consensus(2));
+
+%fprintf('%d\n', score_consensus(1));
+%fprintf('%d\n', score_consensus(2));
+
 figure;
 histogram(-score_train, 'Normalization', 'prob', 'BinWidth', 20);
 hold;
 xlimtrain = get(gca, 'xlim');
-file = fopen('/media/Data/consensus/temp_files/exceptions.txt', 'at');
+
+fname = '/media/Data/consensus/plot_stats/';
+fname_open = strcat(fname, accession);
+fname = strcat(fname_open, '_plot_stats.txt');
+file = fopen(fname, 'w');
+
 histogram(-score_test, 'Normalization', 'prob', 'BinWidth', 20);
+xlimtest = get(gca, 'xlim');
 hold;
 y1 = get(gca, 'ylim');
 line([-score_consensus(1) -score_consensus(1)], y1, 'Color', 'g');
@@ -30,19 +39,41 @@ legend('Refined MSA', 'HMM emitted MSA', 'Consensus from Refined MSA', 'Consensu
 plt = strcat('../dca_energy_plots/', accession);
 plot_name = strcat(plt, '_dca_energies');
 print(plot_name, '-dpng');
-%fprintf('\n\n%d\t', xlimtrain(2));
-%fprintf('%d\n\n', -score_consensus(1));
-%fprintf('%d\n', xlimtrain(2) > -score_consensus(1));
-if xlimtrain(2) < -score_consensus(1)
-	fprintf(file, '%s\n', accession);
-fclose(file);
 
-%file = fopen('/media/Data/consensus/temp_files/score_train.txt', 'w');
-%fprintf(file, '%d\n', score_train);
-%fclose(file);
-%file = fopen('/media/Data/consensus/temp_files/score_test.txt', 'w');
-%fprintf(file, '%d\n', score_test);
-%fclose(file);
+% train stats
+mean_train = -mean(score_train);
+mode_train = -mode(score_train);
+sd_train = std(score_train);
+consensus_train = -score_consensus(1);
+min_x_train = xlimtrain(1);
+max_x_train = xlimtrain(2);
+
+fprintf(file, '%s\n', 'TRAIN STATS (Refined Alignment)');
+fprintf(file, '%s\t%.6f\n', 'Mean: ', mean_train);
+fprintf(file, '%s\t%.6f\n', 'Mode: ', mode_train);
+fprintf(file, '%s\t%.6f\n', 'SD: ', sd_train);
+fprintf(file, '%s\t%.6f\n', 'Consensus energy: ', consensus_train);
+fprintf(file, '%s\t%d\n', 'Min x: ', min_x_train);
+fprintf(file, '%s\t%d\n', 'Max x: ', max_x_train);
+
+% test stats
+mean_test = -mean(score_test);
+mode_test = -mode(score_test);
+sd_test = std(score_test);
+consensus_test = -score_consensus(2);
+min_x_test = xlimtest(1);
+max_x_test = xlimtest(2);
+
+fprintf(file, '%s\n', '-------------------------------------------------------');
+fprintf(file, '%s\n', 'TEST STATS (HMM emitted sequences)');
+fprintf(file, '%s\t%.6f\n', 'Mean: ', mean_test);
+fprintf(file, '%s\t%.6f\n', 'Mode: ', mode_test);
+fprintf(file, '%s\t%.6f\n', 'SD: ', sd_test);
+fprintf(file, '%s\t%.6f\n', 'Consensus energy: ', consensus_test);
+fprintf(file, '%s\t%d\n', 'Min x: ', min_x_test);
+fprintf(file, '%s\t%d\n', 'Max x: ', max_x_test);
+
+fclose(file);
 
 clear eij;
 clear hi;
--- martin_dca/dca_energy.m
+++ martin_dca/dca_energy.m
@@ -1,7 +1,7 @@
 try
 	fasta_test = '/media/Data/consensus/temp_files/only_hmm_emitted.fasta';
 	fasta_train = '/media/Data/consensus/temp_files/train_file.fasta';
-	accession = 'PF06565';
+	accession = 'PF17405';
 	[score_train, score_test] = calculate_dca_scores(fasta_train,fasta_test,accession)
 catch
 	exit
